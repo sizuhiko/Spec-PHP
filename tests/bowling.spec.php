aa33c5eca72983a071f7fe09856b53206f4bbc30
sizuhiko <k-kishida@esm.co.jp>
2012-05-18 13:58:21 +0900

    Change bowling spec using empty it.

--- tests/bowling.spec.php
+++ tests/bowling.spec.php
@@ -30,12 +30,14 @@ end
 
 describe "Bowling#score use subject and it"
   context "all gutter game"
-    subject
-      $bowling = new Bowling;
-      array_filter(range(0, 19), function() use($bowling) { $bowling->hit(0); } );
-      return $bowling->score;
+    before
+      $W->bowling = new Bowling;
+      array_filter(range(0, 19), function() use($W) { $W->bowling->hit(0); } );
     end
-    it "returns 0"
+    subject
+      return $W->bowling->score;
+    end
+    it
       should equal 0
     end
   end
